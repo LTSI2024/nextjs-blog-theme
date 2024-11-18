2efadc0603d1d869dcb80d1803ecab0db65f8316
LTSI2024 <letthatsinkin@gmail.com>
2024-11-18 11:24:04 -0700

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Welcome';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'LetThatSinkInn';
+    : 'LetThatSinkIn';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
